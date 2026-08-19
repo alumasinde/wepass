aa41c957671db75876b356fef2cac14b2dec9513
alumasinde <143938688+alumasinde@users.noreply.github.com>
2026-08-19 21:49:11 +0300

    security: harden visitor policy

--- src/Modules/Visitors/Policies/VisitorPolicy.php
+++ src/Modules/Visitors/Policies/VisitorPolicy.php
@@ -1,38 +1,78 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Modules\Visitors\Policies;
 
 use App\Core\Permission;
 
-class VisitorPolicy
+final class VisitorPolicy
 {
-     public function __construct(
-        private Permission $permission
-    ) {}
+    public function __construct(private readonly Permission $permission)
+    {
+    }
 
     public function view(): bool
     {
-        return $this->permission->can(  'visitors.view');
+        return $this->permission->can('visitors.view')
+            || $this->permission->can('visitors.view_all');
     }
 
     public function create(): bool
     {
-        return $this->permission->can(  permission: 'visitors.create');
+        return $this->permission->can('visitors.create');
     }
 
     public function update(): bool
     {
-        return $this->permission->can(   'visitors.update');
+        return $this->permission->can('visitors.update')
+            || $this->permission->can('visitors.update_all');
     }
 
-   public function delete(): bool
+    public function delete(): bool
     {
-        return $this->permission->can(   'visitors.delete');
+        return $this->permission->can('visitors.delete');
     }
 
     public function issueBadge(): bool
     {
-        return $this->permission->can(   'visitors.issue_badge');
+        return $this->permission->can('visitors.issue_badge')
+            || $this->permission->can('badges.issue');
     }
 
+    public function canViewRecord(array $visitor): bool
+    {
+        if ($this->permission->can('visitors.view_all')) {
+            return true;
+        }
+
+        return $this->permission->can('visitors.view')
+            && $this->ownedByCurrentUser($visitor);
+    }
+
+    public function canUpdateRecord(array $visitor): bool
+    {
+        if ($this->permission->can('visitors.update_all')) {
+            return true;
+        }
+
+        return $this->permission->can('visitors.update')
+            && $this->ownedByCurrentUser($visitor);
+    }
+
+    public function canBlacklistRecord(array $visitor): bool
+    {
+        if ($this->permission->can('visitors.manage')) {
+            return true;
+        }
+
+        return $this->permission->can('visitors.blacklist')
+            && $this->ownedByCurrentUser($visitor);
+    }
+
+    private function ownedByCurrentUser(array $visitor): bool
+    {
+        $userId = (int) ($_SESSION['user']['id'] ?? 0);
+        return $userId > 0 && (int) ($visitor['created_by'] ?? 0) === $userId;
+    }
 }
